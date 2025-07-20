7f9dedeebbe0aeb93e0e0c8cefde7da2b85d08da
Rai Yashasvee Srivastav <rai.srivastav@hotmail.com>
2025-07-20 22:08:59 +0530

    Add MoveZeroes and Rotate functions for array manipulation

--- MoveZeroes.cs
+++ MoveZeroes.cs
@@ -1,1 +1,40 @@
- 
+ public class Program
+{
+    public static void MoveZeroes(int[] nums)
+    {
+        int j = -1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] == 0)
+            {
+                j=i; 
+                break;
+            }
+        }
+
+        if(j==-1) return;
+
+        for (int i = j; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+
+
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] nums = [3, 0, 1, 0, 3, 12];
+        MoveZeroes(nums);
+
+        foreach (int num in nums) {
+            Console.WriteLine(num);
+        }
+    }
+}
